b23bfb340db15506a3fc830590e07aa33b4a518a
David Wang <dwang2164@gmail.com>
2026-05-20 13:54:23 -0500

    initalize go backend websocket

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ To do:
 -create websocket server that sends odds maybe once every few seconds
 -update cards live on front end with this
 -replace with real data
+
+Backend Go Setup:
+go mod init backend (in backend folder)
+go get github.com/gorilla/websocket (install websocket pkg)
+
+Run backend:
+go run main.go
+
+Build into native program:
+go build
---
+++ backend/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+// initialize global random number generator with current time as seed to ensure different odds each time server starts
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// create odds struct to send to frontend
+type Odds struct {
+	Player string `json:"player"`
+	Market string `json:"market"`
+	Odds   int    `json:"odds"`
+}
+
+// initialize websocket upgrader to keep socket open with default options
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// simulate random odds to change every 3 seconds and send to frontend
+func randomOdds() []Odds {
+	return []Odds{
+		{
+			Player: "LeBron James",
+			Market: "Over 6.5 Rebounds",
+			Odds:   -230 + r.Intn(20),
+		},
+		{
+			Player: "Stephen Curry",
+			Market: "Over 5.5 Assists",
+			Odds:   -200 + r.Intn(20),
+		},
+		{
+			Player: "Alperen Sengun",
+			Market: "Over 20.5 Points",
+			Odds:   -215 + r.Intn(20),
+		},
+	}
+}
+
+// handle websocket connection and send random odds every 3 seconds
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer conn.Close() //run before function exits to ensure connection is closed
+
+	// continuously send random odds to frontend every 3 seconds until connection is closed
+	for {
+		data := randomOdds()
+
+		err := conn.WriteJSON(data)
+
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func main() {
+	//websocket endpoint /ws to handle connections and send odds
+	http.HandleFunc("/ws", wsHandler)
+
+	log.Println("Server running on :8080")
+
+	//start server on port 8080 and log any errors
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
